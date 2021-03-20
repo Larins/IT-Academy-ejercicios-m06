da31c5c4dea496fa6524b4f56e8ae356bab93833
Larins <69240139+Larins@users.noreply.github.com>
2021-03-20 03:07:59 +0100

    Subida ejercicios Nivel 02 validación if-else

--- Nivel 02/src/Domain/Employee.java
+++ Nivel 02/src/Domain/Employee.java
@@ -7,6 +7,15 @@ public class Employee {
 	public String name;
 	public String address;
 	public String phone;
-	double Salary = 1000;
+	
+	//Suponemos un sueldo base de 1000€ para todos los empleados.
+	//Cada tipo de empleado heredará este sueldo base.
+	//Cada tipo de empleado tiene en su clase un modificador de sueldo según su rango en la jerarquía de la empresa.
+			//Por ejemplo: la clase BOSS tiene un modificador de 1.5 porque su sueldo debe ser un 50% mayor.
+	//En la clase MAIN multiplicaremos el sueldo base por el modificador para ver su sueldo final.
+	//Este sueldo final nos servirá para ver si se cumple la condición de sueldo.
+			//Por ejemplo: la clase BOSS debería tener un sueldo superior a 8000€.
+	
+	double Salary = 3000;
 	
 }
--- Nivel 02/src/Domain/Main.java
+++ Nivel 02/src/Domain/Main.java
@@ -1,52 +1,99 @@
 package Domain;
 
 public class Main {
+	
+	//En las clase EMPLOYEE hemos generado los atributos generales de los empleados
+	//En las otras clases de empleados hemos generado sus atributos específicos
+	//En esta clase MAIN vamos a crear unos cuantos empleados de diferentes categorías
+	//Vamos a pintar sus atributos
+	//Vamos a comprobar que su sueldo se ajusta a su categoría
+			//El sueldo esta calculado de la siguiente manera
+			//En la clase EMPLOYEE se le da un valor al sueldo base (SALARY)
+			//En las otras clases de empleados se da un valor a los modificadores por categoría (por ejemplo PLUSBOSS)
+			//En esta clase MAIN calculamos el sueldo como SALARY*PLUSSBOSS
+			//Si se quieren hacer comprobaciones del sistema de validación, se puede cambiar el valor de SALARY desde la clase EMPLOYEE
 
 	public static void main(String[] args) {
 
-		Boss PepeBoss = new Boss();
-		PepeBoss.name = "Pepe Boss";
-		PepeBoss.address = "Calle Molona Bla Bla";
-		PepeBoss.phone = "123456789";
+		Boss FranciscoBoss = new Boss();
+		FranciscoBoss.name = "Francisco Boss";
+		FranciscoBoss.address = "Calle Molona Bla Bla";
+		FranciscoBoss.phone = "123456789";
 
 		Manager PedroManager = new Manager();
 		PedroManager.name = "Pedro Employee";
 		PedroManager.address = "Calle Molona Bla Bla";
 		PedroManager.phone = "123456789";
 
-		Senior LauraSenior = new Senior();
-		LauraSenior.name = "Laura Senior";
-		LauraSenior.address = "Calle Molona Bla Bla";
-		LauraSenior.phone = "123456789";
+		Senior AlejandraSenior = new Senior();
+		AlejandraSenior.name = "Alejandra Senior";
+		AlejandraSenior.address = "Calle Molona Bla Bla";
+		AlejandraSenior.phone = "123456789";
 
-		Mid JosechuMid = new Mid();
-		JosechuMid.name = "Josechu Mid";
-		JosechuMid.address = "Calle Molona Bla Bla";
-		JosechuMid.phone = "123456789";
+		Mid ErmensindaMid = new Mid();
+		ErmensindaMid.name = "Ermensinda Mid";
+		ErmensindaMid.address = "Calle Molona Bla Bla";
+		ErmensindaMid.phone = "123456789";
 
-		Junior MartaJunior = new Junior();
-		MartaJunior.name = "Marta Junior";
-		MartaJunior.address = "Calle Molona Bla Bla";
-		MartaJunior.phone = "123456789";
+		Junior CarlotaJunior = new Junior();
+		CarlotaJunior.name = "Carlota Junior";
+		CarlotaJunior.address = "Calle Molona Bla Bla";
+		CarlotaJunior.phone = "123456789";
 
 		Volunteer JuanVolunteer = new Volunteer();
 		JuanVolunteer.name = "Juan Volunteer";
 		JuanVolunteer.address = "Calle Molona Bla Bla";
 		JuanVolunteer.phone = "123456789";
 
-		System.out.println("Empleado: " + PepeBoss.name + "\nDir: " + PepeBoss.address + "\nTel: " + PepeBoss.phone
-				+ "\nSueldo: " + PepeBoss.Salary * PepeBoss.PlusBoss + " €\n");
-		System.out.println("Empleado: " + PedroManager.name + "\nDir: " + PedroManager.address + "\nTel: "
-				+ PedroManager.phone + "\nSueldo: " + PedroManager.Salary * PedroManager.PlusManager + " €\n");
-		System.out.println("Empleado: " + LauraSenior.name + "\nDir: " + LauraSenior.address + "\nTel: "
-				+ LauraSenior.phone + "\nSueldo: " + LauraSenior.Salary * LauraSenior.PlusSenior + " €\n");
-		System.out.println("Empleado: " + JosechuMid.name + "\nDir: " + JosechuMid.address + "\nTel: "
-				+ JosechuMid.phone + "\nSueldo: " + JosechuMid.Salary * JosechuMid.PlusMid + " €\n");
-		System.out.println("Empleado: " + MartaJunior.name + "\nDir: " + MartaJunior.address + "\nTel: "
-				+ MartaJunior.phone + "\nSueldo: " + MartaJunior.Salary * MartaJunior.PlusJunior + " €\n");
-		System.out.println("Empleado: " + JuanVolunteer.name + "\nDir: " + JuanVolunteer.address + "\nTel: "
-				+ JuanVolunteer.phone + "\nSueldo: " + JuanVolunteer.Salary * JuanVolunteer.PlusVolunteer + " €\n");
+		//BOSS
+		System.out.println("Empleado: " + FranciscoBoss.name + "\tDir: " + FranciscoBoss.address + "\tTel: "
+				+ FranciscoBoss.phone + "\t\tSueldo: " + FranciscoBoss.Salary * FranciscoBoss.PlusBoss + " €\t");
+		if (FranciscoBoss.Salary * FranciscoBoss.PlusBoss>8000){
+			System.out.println("El sueldo está en la franja correcta\n");
+		}else{
+			System.out.println("El sueldo es incorrecto\nDebería ser superior a 8000€\n");}
+		
+		//MANAGER
+		System.out.println("Empleado: " + PedroManager.name + "\tDir: " + PedroManager.address + "\tTel: "
+				+ PedroManager.phone + "\t\tSueldo: " + PedroManager.Salary * PedroManager.PlusManager + " €\t");
+		if (PedroManager.Salary * PedroManager.PlusManager>3000 && PedroManager.Salary * PedroManager.PlusManager<5000){
+			System.out.println("El sueldo está en la franja correcta\n");
+		}else{
+			System.out.println("El sueldo es incorrecto\nDebería estar entre 3000€ y 5000€\n");}
+		
+		//SENIOR
+		System.out.println("Empleado: " + AlejandraSenior.name + "\tDir: " + AlejandraSenior.address + "\tTel: "
+				+ AlejandraSenior.phone + "\t\tSueldo: " + AlejandraSenior.Salary * AlejandraSenior.PlusSenior + " €\t");
+		if (AlejandraSenior.Salary * AlejandraSenior.PlusSenior>2700 && AlejandraSenior.Salary * AlejandraSenior.PlusSenior<4000){
+			System.out.println("El sueldo está en la franja correcta\n");
+		}else{
+			System.out.println("El sueldo es incorrecto\nDebería estar entre 2700€ y 4000€\n");}
+		
+		//MID
+		System.out.println("Empleado: " + ErmensindaMid.name + "\tDir: " + ErmensindaMid.address + "\tTel: "
+				+ ErmensindaMid.phone + "\t\tSueldo: " + ErmensindaMid.Salary * ErmensindaMid.PlusMid + " €\t");
+		if (ErmensindaMid.Salary * ErmensindaMid.PlusMid>1800 && ErmensindaMid.Salary * ErmensindaMid.PlusMid<2500){
+			System.out.println("El sueldo está en la franja correcta\n");
+		}else{
+			System.out.println("El sueldo es incorrecto\nDebería estar entre 1800€ y 2500€\n");}
+		
+		//JUNIOR
+		System.out.println("Empleado: " + CarlotaJunior.name + "\tDir: " + CarlotaJunior.address + "\tTel: "
+				+ CarlotaJunior.phone + "\t\tSueldo: " + CarlotaJunior.Salary * CarlotaJunior.PlusJunior + " €\t");
+		if (CarlotaJunior.Salary * CarlotaJunior.PlusJunior>900 && CarlotaJunior.Salary * CarlotaJunior.PlusJunior<1600){
+			System.out.println("El sueldo está en la franja correcta\n");
+		}else{
+			System.out.println("El sueldo es incorrecto\nDebería estar entre 900€ y 1600€\n");}
+		
+		//VOLUNTEER
+		System.out.println("Empleado: " + JuanVolunteer.name + "\tDir: " + JuanVolunteer.address + "\tTel: "
+				+ JuanVolunteer.phone + "\t\tSueldo: " + JuanVolunteer.Salary * JuanVolunteer.PlusVolunteer + " €\t");
+		if (JuanVolunteer.Salary * JuanVolunteer.PlusVolunteer<=0){
+			System.out.println("El sueldo está en la franja correcta");
+		}else{
+			System.out.println("El sueldo es incorrecto\nDebería ser inferior a 0€\n");}
 
+		
 	}
 
 }
